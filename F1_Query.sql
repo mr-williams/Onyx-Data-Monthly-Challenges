1f9dededc337956850fdad5bc33e0658e1131f14
mr-williams <70599292+mr-williams@users.noreply.github.com>
2022-04-15 19:22:35 +0000

    Update F1_Query.sql

--- F1_Query.sql
+++ F1_Query.sql
@@ -1,3 +1,11 @@
+/*This Query Shows the number of Pole positions a driver has gotten*/
+SELECT (CONCAT_WS(' ',[Driver's forename],[Driver's surname])) AS Full_Name, COUNT(grid) AS Pole_Position
+FROM results$
+WHERE grid = 1
+GROUP BY grid,[Driver's forename],[Driver's surname]
+ORDER BY Pole_Position DESC
+
+
 --- This Query Shows the top 10 most succesful drivers based on their wins ---
 SELECT (CONCAT_WS(' ',[Driver's forename],[Driver's surname])) AS Full_Name, COUNT(position) AS RACE_WINS_
 FROM results$
@@ -35,7 +43,37 @@ SELECT COUNT(DISTINCT(CONCAT_WS(' ',[Driver's forename],[Driver's surname]))) AS
        DATEDIFF(yy,'1950/05/13','2021/12/12') AS Years_of_F1,
        COUNT(DISTINCT([Event name])) AS Venues
 FROM results$
+                                      
+ /* This Query shows and counts how many Driver nationalities have competed in the sport*/                                     
+SELECT DISTINCT([Driver's nationality]), COUNT(DISTINCT((CONCAT_WS(' ', [Driver's forename],[Driver's surname])))) AS Nationality_Appearance
+FROM results$
+GROUP BY [Driver's nationality]
+ORDER BY Nationality_Appearance DESC
+            
+ /*This Query Shows How many different constructor nationalities have competed*/              
+SELECT DISTINCT([constructor's nationality]),COUNT(DISTINCT([Constructor name])) Nationality_Appearance_Constructors
+FROM results$
+GROUP BY [constructor's nationality]
+ORDER BY Nationality_Appearance_Constructors DESC
 
 
+/*This Query shows how many races in a season in all years of F1*/
+SELECT year, COUNT(DISTINCT([Event name])) AS Number_of_races
+FROM results$
+GROUP BY year
+ORDER BY Number_of_races                
 
 
+/*This query shows which nationality has the most wins for drivers*/
+SELECT DISTINCT([Driver's nationality]), COUNT([Driver's nationality]) AS nationality_wins
+FROM results$
+WHERE positionOrder = 1
+GROUP BY [Driver's nationality]'
+ORDER BY nationality_wins DESC
+
+/*This query shows which nationality has the most wins for constructors*/
+SELECT DISTINCT([constructor's nationality]), COUNT([constructor's nationality]) AS nationality_wins
+FROM results$
+WHERE positionOrder = 1
+GROUP BY [constructor's nationality]
+ORDER BY nationality_wins DESC
